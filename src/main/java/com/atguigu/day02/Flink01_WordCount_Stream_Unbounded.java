54bf0dd7fce318a3148ee2333734a2da4e869303
0826 <0826@atguigu.com>
2022-01-10 11:06:31 +0800

    day02上午

--- src/main/java/com/atguigu/day02/Flink01_WordCount_Stream_Unbounded.java
+++ src/main/java/com/atguigu/day02/Flink01_WordCount_Stream_Unbounded.java
@@ -1,22 +1,29 @@
-package com.atguigu.day01;
+package com.atguigu.day02;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
 
-public class Flink03_WordCount_Stream_Unbounded {
+public class Flink01_WordCount_Stream_Unbounded {
     public static void main(String[] args) throws Exception {
         //1.获取流的执行环境
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+//        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        //通过这种获取执行环境的方式可以在本地通过访问 localhost:8081来访问UI界面
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
 
         //并行度设置为1
-//        env.setParallelism(2);
+        env.setParallelism(1);
+
+        //全局都不串
+        env.disableOperatorChaining();
 
         //2.读取无界数据
         DataStreamSource<String> streamSource = env.socketTextStream("hadoop102", 9999);
@@ -30,7 +37,13 @@ public class Flink03_WordCount_Stream_Unbounded {
                     out.collect(word);
                 }
             }
-        });
+        })
+//                .setParallelism(3)
+                //与前面都断开
+//                .startNewChain()
+                //与前后都断开
+//                .disableChaining()
+                ;
 
         //4.将数据封装为Tuple2元组
         SingleOutputStreamOperator<Tuple2<String, Integer>> wordToOneDStream = wordDStream.map(new MapFunction<String, Tuple2<String, Integer>>() {
